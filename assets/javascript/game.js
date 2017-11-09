d218fc0379ddb2e748f96589107a6bc42c2790bb
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-11-09 10:02:09 -0600

    begin sort of again

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //Possible CP picks
-var options:Array = ("abcdefghijklmnopqrstuvwxyz").split("");
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z'"];
 
 //Game status. Wins and losses set to 0 and guessesLeft set to 9
 var wins = 0;
@@ -8,25 +8,37 @@ var guessesLeft = 9;
 var guessesMade = [];
 var PCpick = null;
 
+
 //Coputer chooses one letter from the avaliable choices.
-var computerGuess = options[Math.floor(Math.random() * options.length)];
+// var computerGuess = options[Math.floor(Math.random() * options.length)];
+var index = Math.floor(Math.random() * options.length);
+var PCpick = options[index];
+console.log(PCpick);
 
-//Player begins with 9 lives.
-function guessesRemaining() {
-	document.querySelector('#guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
-};
 
-var chosenLetter = function() {
-  this.PCpick = this.options[Math.floor(Math.random() * this.options.length)];
-};
+// //Player begins with 9 lives.
+// function guessesRemaining() {
+// 	document.querySelector('#guessesLeft').innerHTML = "Guesses left: " + guessesLeft;
+// };
 
-//Attempted letter get displaid separated by a comma everytime.
-function lettersSaid() {
-	document.querySelector('#haveSaid').innerHTML = "Your guesses so far: " + guessedLetters.join(',');
+document.getElementById("wins").textContent = "Wins: " + wins;
+
+document.getElementById("losses").textContent = "Losses " + losses;
+
+document.getElementById("guessesLeft").textContent = "Guesses left: " + guessesLeft;
+
+document.getElementById("haveSaid").textContent = "Your guesses so far: " + guessesMade;
+
+//Attempted letter gets display
+function lettersPicked() {
+	document.getElementById("haveSaid").textContent = "Your guesses so far: " + guessedLetters.join(" ");
 };
 
 //This function will be called to reset the game
 function reset() {
+	if ((guessesLeft < 1) ||  {
+
+	}
 	guessesLeft = 9;
 	guessesMade = [];
 
